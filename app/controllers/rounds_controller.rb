f6bd1f4618f07a36a749ca17d567eb0337c3ec76
Matt McQuinn <mmcquinn77@gmail.com>
2016-12-19 20:23:12 -0600

    Modify create method for rounds
    Add authentication so that users can only create their own rounds

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,2 +1,27 @@
 class RoundsController < ApplicationController
+  before_action :authenticate_user, only: [:index, :show]
+  # only the current user can edit and create their own rounds
+  before_action :authenticate, only: [:update, :delete]
+
+  def create
+    if params[:data] && params[:data][:attributes]
+      user_id = params[:data][:attributes][:'user-id']
+      user = User.find(user_id)
+      if user == current_user
+        super
+      else
+        head :unauthorized
+      end
+    else
+      head :unauthorized
+    end
+  end
+
+  private
+    def authenticate
+      round = Round.find(params[:id])
+      user = User.find(round.user_id)
+      head :unauthorized unless user == current_user
+    end
+
 end
