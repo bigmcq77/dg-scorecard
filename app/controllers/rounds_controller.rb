d51dadb4333336147899b497c921b89f721cdaef
Matt McQuinn <mmcquinn77@gmail.com>
2016-12-19 20:42:53 -0600

    Refactor rounds create method

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,27 +1,30 @@
 class RoundsController < ApplicationController
   before_action :authenticate_user, only: [:index, :show]
-  # only the current user can edit and create their own rounds
+  # only the current user can edit and update their own rounds
   before_action :authenticate, only: [:update, :delete]
+  before_action :load_user, only: [:create]
 
   def create
-    if params[:data] && params[:data][:attributes]
-      user_id = params[:data][:attributes][:'user-id']
-      user = User.find(user_id)
-      if user == current_user
-        super
-      else
-        head :unauthorized
-      end
+    if @user == current_user
+      super
     else
       head :unauthorized
     end
   end
 
   private
-    def authenticate
-      round = Round.find(params[:id])
-      user = User.find(round.user_id)
-      head :unauthorized unless user == current_user
-    end
+  def authenticate
+    round = Round.find(params[:id])
+    user = User.find(round.user_id)
+    head :unauthorized unless user == current_user
+  end
 
+  def load_user
+    if params[:data] && params[:data][:attributes]
+      user_id = params[:data][:attributes][:'user-id']
+      @user = User.find(user_id)
+    else
+      head :unauthorized
+    end
+  end
 end
