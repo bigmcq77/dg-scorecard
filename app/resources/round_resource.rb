e87be61dc4af5ab1e7252242e035263fb3086888
Matt McQuinn <mmcquinn77@gmail.com>
2016-12-19 20:21:37 -0600

    Add course and user ids to round resource

--- app/resources/round_resource.rb
+++ app/resources/round_resource.rb
@@ -1,5 +1,10 @@
 class RoundResource < JSONAPI::Resource
-  attributes :weather
+  attributes :weather, :user_id, :course_id
   has_many :scores
   has_one :user
+  has_one :course
+
+  def fetchable_fields
+    super - [:user_id, :course_id]
+  end
 end
