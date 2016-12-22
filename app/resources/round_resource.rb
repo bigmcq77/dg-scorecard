e789683486141538db546044a6a35f73fa928de8
Matt McQuinn <mmcquinn77@gmail.com>
2016-12-21 22:30:59 -0600

    Remove code for checking round creation
    Remove user_id and course_id attributes in resource file
    Now that we are using the relationships key properly this is not
    needed anymore! :D

--- app/resources/round_resource.rb
+++ app/resources/round_resource.rb
@@ -1,10 +1,10 @@
 class RoundResource < BaseResource
-  attributes :weather, :user_id, :course_id
+  attributes :weather
   has_many :scores
   has_one :user
   has_one :course
 
-  def fetchable_fields
-    super - [:user_id, :course_id]
-  end
+  # def fetchable_fields
+  #   super - [:user_id, :course_id]
+  # end
 end
