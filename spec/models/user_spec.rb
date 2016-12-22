6762b828685147050b56a23838794d56ebecdd80
Matt McQuinn <mmcquinn77@gmail.com>
2016-12-22 01:06:41 -0600

    Validate presense of user names and emails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   it { should respond_to :name }
   it { should respond_to :email }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :email }
 end
