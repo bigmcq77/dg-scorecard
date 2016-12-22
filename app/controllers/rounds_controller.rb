a2ad241a328cdfa1c0081a477f59ba49a83805cc
Matt McQuinn <mmcquinn77@gmail.com>
2016-12-21 19:30:28 -0600

    Authorize round creation

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,2 +1,29 @@
 class RoundsController < ApplicationController
+  before_action :load_user, only: :create
+
+  def create
+    # only the current user should be able to create their own rounds
+    if @user == current_user
+      super
+    else
+      head :unauthorized
+    end
+  end
+
+  private
+
+    def load_user
+      if params[:data] && params[:data][:attributes]
+        user_id = params[:data][:attributes][:'user-id']
+        if User.exists?(id: user_id)
+          @user = User.find(user_id)
+        else
+          # user_id wasn't valid
+          head :bad_request
+        end
+      else
+        # user_id wasn't passed in
+        head :bad_request
+      end
+    end
 end
