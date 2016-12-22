1bef81db5b175ec6ebe8fdc390842689303baf24
Matt McQuinn <mmcquinn77@gmail.com>
2016-12-21 18:59:42 -0600

    Remove commented code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,2 @@
 class UsersController < ApplicationController
-  # Only auth'd users can view the list of users and a user page
-  # before_action :authenticate_user, only: [:index, :show]
-  # # Only the current user can view and edit their self
-  # before_action :authenticate, only: [:destroy, :update]
-
-  # private
-  #   def authenticate
-  #     user = User.find(params[:id])
-  #     head :unauthorized unless user == current_user
-  #   end
-  #
 end
