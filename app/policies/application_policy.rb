7b074b36b37e8812572c22ede309cae9f8cd9e11
Matt McQuinn <mmcquinn77@gmail.com>
2016-12-21 18:59:23 -0600

    Remove forbidden response if the user is nil

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -2,7 +2,6 @@ class ApplicationPolicy
   attr_reader :user, :record
 
   def initialize(user, record)
-    raise Pundit::NotAuthorizedError unless user
     @user = user
     @record = record
   end
