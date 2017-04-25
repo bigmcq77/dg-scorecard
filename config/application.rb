06d403834051701a98896dd87b32ab34552e8e8d
Matthew McQuinn <mmcquinn77@gmail.com>
2017-04-25 14:27:56 -0500

    Enable CORS and change database names

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,15 @@ module DgScorecard
     end
 
     config.api_only = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
