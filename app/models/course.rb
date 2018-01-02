532cc31eb628875dc8889235a4b891b345557c96
Matthew McQuinn <mmcquinn77@gmail.com>
2018-01-01 19:23:27 -0700

    Don't auto create holes when creating a course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,13 +1,6 @@
 class Course < ApplicationRecord
   has_many :holes
-  after_create :init_holes
 
   validates :name, presence: true
   validates :num_holes, numericality: { greater_than: 0 }
-
-  def init_holes
-    self.num_holes.times do |i|
-      self.holes.create!(number: i+1, par: 3)
-    end
-  end
 end
