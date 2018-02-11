118f47ed2f78e51135a53b8bb5a99cbd6e37dcc6
Matthew McQuinn <mmcquinn77@gmail.com>
2018-02-11 13:15:05 -0700

    Auto provision scores when a new round is started
    Delete scores when a round is deleted
    Turn off pagination for rounds

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,5 +2,13 @@ class Round < ApplicationRecord
   belongs_to :user
   belongs_to :course
 
-  has_many :scores
+  has_many :scores, :dependent => :destroy
+
+  after_create :init_scores
+
+  def init_scores
+    self.course.holes.each do |hole|
+      self.scores.create!(hole: hole, round: self, strokes: hole.par)
+    end
+  end
 end
--- app/resources/round_resource.rb
+++ app/resources/round_resource.rb
@@ -3,4 +3,8 @@ class RoundResource < BaseResource
   has_many :scores
   has_one :user
   has_one :course
+ 
+  paginator :none
+
+  filter :user
 end
